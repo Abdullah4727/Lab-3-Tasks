90bbca80f02f6c6f67034257c1468744b617deb1
Abdullah4727 <abdullah.syed4727@gmail.com>
2025-03-25 21:19:15 +0500

    Add files via upload

---
+++ Error Identification.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "The result is: " << 5 * 3 << endl;
+	cout << "Corrected: " << 5 + 3 << endl; /* Fixed the line by adding a second integer after the operator.
+	Added the missing semi colon at the end of the line.*/
+	return 0; //Added return statement.
+}
