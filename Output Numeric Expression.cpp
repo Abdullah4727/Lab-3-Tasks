1721a15753f6630b4b0ce936fa1225310c68d12b
Abdullah4727 <abdullah.syed4727@gmail.com>
2025-03-25 09:44:28 +0500

    Previous file with comments

--- Output Numeric Expression.cpp
+++ Output Numeric Expression.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-using namespace std;
+#include <iostream> 
+using namespace std; /*The std namespace contains many standard library components.
+These include data types, functions objects.*/
 int main() {
-	cout << 2 + 3 * 4 << endl;
+	cout << 2 + 3 * 4 << endl; //in C++ multiplication has precedence over addition
 	return 0;
 }
