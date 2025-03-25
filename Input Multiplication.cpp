b58c29300f9f8120131c4072cbfda70304deb26c
Abdullah4727 <abdullah.syed4727@gmail.com>
2025-03-25 21:18:46 +0500

    Add files via upload

---
+++ Input Multiplication.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num1, num2, product;
+
+	cout << "Enter the first number: ";
+	cin >> num1;
+
+	cout << "Enter the second number: ";
+	cin >> num2;
+
+	product = num1 * num2;
+
+	cout << "The product is: " << product << endl;
+	return 0;
+}
