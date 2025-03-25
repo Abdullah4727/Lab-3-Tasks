3bbf2018cbcd46c3eec6f0d145cec5fdc16da44f
Abdullah4727 <abdullah.syed4727@gmail.com>
2025-03-25 21:18:09 +0500

    Add files via upload

---
+++ Variable Product.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int x = 36;
+	int y = 22;
+	int product = x * y;
+	cout << "The product of the two variables is: " << product << endl;
+	return 0;
+}
